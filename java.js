6feb623eae8b3c0c5e67a0f2d0062d4cb992d229
AA07 <alexanderadame.david@gmail.com>
2022-08-18 12:46:01 -0500

    Formatting,Final, Changes

--- java.js
+++ java.js
@@ -1,110 +1,98 @@
+
 let title = document.getElementById("title");
 let clock = document.getElementById("clock");
-let date = document.getElementById("date")
+let date = document.getElementById("date");
+title.innerHTML = "A Ticking Clock!"
+
 function ticky() {
-  const live = new Date()
-  const h = live.getHours();
-  const m = live.getMinutes();
-  const s = live.getSeconds();
-  const wd = live.getDay();
-  const wn = live.getDate();
-  const mt = live.getMonth();
-  const yr = live.getFullYear();
-  date.innerHTML = (weekday(wd)+","+months(mt)+suffix(wn)+(yr));
-  clock.innerHTML = (militarytostandard(h)+":"+mino(m)+":"+mono(s)); 
-    title.innerHTML = "A Ticking Clock!"
+    const live = new Date()
+    const h = live.getHours();
+    const m = live.getMinutes();
+    const s = live.getSeconds();
+    const wd = live.getDay();
+    const wn = live.getDate();
+    const mt = live.getMonth();
+    const yr = live.getFullYear();
+
+    clock.innerHTML = (militarytostandard(h)+":"+leadZero(m)+":"+leadZero(s)+" "+ ampm(h) );
+    date.innerHTML = (weekday(wd)+", "+months(mt)+" "+suffix(wn)+" "+(yr));
+}
+
 function militarytostandard(military) {
-  if ( h === 0){
-    return 12
+    let tempH = military
+    if ( tempH === 0){
+        tempH = 12
     } 
-    else if (h > 12) {
-    return h - 12
-    } 
-   else if  (h < 10) {
-    return "0" + h 
+    else if (tempH > 12) {
+        tempH =  tempH - 12
+    }
+
+    if  (tempH < 10) {
+        return "0" + tempH 
     }
     else {
-      return h
+        return tempH
     }
-  }
-function Time(st){
-  if (st<10){
-    return"0"+st
-   }
-  }
-function mono(ma){
-  if (s<10){
-    return "0"+s
+}
+
+function leadZero(ma){
+    if (ma<10){
+        return "0"+ma
     }
-  else if (h<12){
-    return s+"AM";
+    else {
+        return ma
     }
-  else if (h>12){
-    return s+"PM";
-    }
-  else {
-    return s
-    }
-  }  
-function mino(mo){
-  if (m<10){
-    return"0"+m
-    }
-  else {
-    return m
-    }
-  }
+} 
+
 function ampm(tod){
-  if (h<12){
-    return s+"AM";
-    }
-  if (h>12){
-    return s+"PM";
-    }
-  }
+    return (tod<12 ? "AM" : "PM");
+}
+
 function weekday(index) {
     const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
     ];
   return days[index];
-  }
+}
+
 function suffix(sfx){
-  if (sfx === 1 || date === 21 || date === 31){
-    return sfx + "st";
+    if (sfx === 1 || date === 21 || date === 31){
+        return sfx + "st";
     } 
     else if (sfx === 2 || sfx === 22){
-    return sfx + "nd";
+        return sfx + "nd";
     }
     else if (sfx === 3 || sfx === 23){
-    return sfx + "rd";
+        return sfx + "rd";
     }
     else {
-      return sfx + "th";
+        return sfx + "th";
     }
-  }
+}
+
 function months(index) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ];
     return months [index];
-  }
 }
+
 setInterval (ticky, 100);
 ticky();
